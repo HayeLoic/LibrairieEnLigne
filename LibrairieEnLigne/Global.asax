fe275d3461b0cfa9d49ea7cb36bc976eaae96949
LHAYE <lhaye@avocatparis.org>
2016-11-02 15:27:03 +0100

    Création de la structure de base de l'application

---
+++ LibrairieEnLigne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibrairieEnLigne.MvcApplication" Language="C#" %>
---
+++ LibrairieEnLigne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibrairieEnLigne.Startup))]
+namespace LibrairieEnLigne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
